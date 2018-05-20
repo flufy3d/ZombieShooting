b2ea88fb2cb8f1adbbd3efd176a06bc0d30dfce5
flufy3d <flufy3d@gmail.com>
2018-05-20 15:35:56 +0200

    OutSide UI complement

---
+++ Assets/Scripts/UI/OutSideUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutSideUI : MonoBehaviour {
+
+    public Animator panelControl;
+    private string prevState;
+    private string state;
+	// Use this for initialization
+	void Start () {
+        prevState = "hide";
+        state = "hide";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        Vector2 mouseOnScreenPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        if (mouseOnScreenPos.y < 0.15f)
+        {
+            state = "show";
+            if(prevState == "hide")
+                panelControl.SetTrigger("show");
+            
+        }
+        else
+        {
+            state = "hide";
+            if(prevState == "show")
+                panelControl.SetTrigger("hide");
+            
+        }
+
+        prevState = state;
+	}
+}
